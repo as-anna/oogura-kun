a54a7805cb18c641c60fd6da53103a2eccec9f0b
Anna A <patty.asprer@gmail.com>
2019-12-22 14:45:10 -0800

    He now tells current pst time

--- bot.py
+++ bot.py
@@ -1,16 +1,42 @@
 import os
 import discord
 from dotenv import load_dotenv
+from discord.ext import commands
+from datetime import datetime
+
 
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
 GUILD = os.getenv('DISCORD_GUILD')
 
 client = discord.Client()
+bot = commands.Bot(command_prefix='ogu ')
+
+
+@bot.event
+async def on_ready():
+    print(f'{bot.user.name} is connected to Discord :D')
+
 
 @client.event
-async def on_ready():
-    guild = discord.utils.find(lambda g: g.name == GUILD, client.guilds)
-    print(f'{client.user} is connected to the following guilds:\n{guild.name}(id: {guild.id})')
+async def on_member_join(member):
+    await member.create_dm()
+    await member.dm_channel.send(
+        f'Hi {member.name}, welcome to {GUILD}'
+    )
 
-client.run(TOKEN)
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+    if 'tadaima' in message.content.lower():
+        await message.channel.send("Okaeri!")
+
+
+@bot.command(name='time')
+async def time(ctx):
+    current_time = datetime.now().strftime("%H:%M:%S")
+    await ctx.send(current_time)
+
+bot.run(TOKEN)

a5e79b1f4e706a959f311df05d90cd3c7c35cb11
Anna A <patty.asprer@gmail.com>
2019-12-23 12:58:06 -0800

    Now has 'Playing' status

--- bot.py
+++ bot.py
@@ -3,11 +3,14 @@ import discord
 from dotenv import load_dotenv
 from discord.ext import commands
 from datetime import datetime
+import pytz
 
 
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
 GUILD = os.getenv('DISCORD_GUILD')
+
+tz_names = dict([(pytz.timezone(x).localize(datetime.now()).tzname(), x) for x in pytz.all_timezones])
 
 # client = discord.Client()
 bot = commands.Bot(command_prefix='ogu ')
@@ -15,6 +18,7 @@ bot = commands.Bot(command_prefix='ogu ')
 
 @bot.event
 async def on_ready():
+    await bot.change_presence(status=discord.Status.online, activity=discord.Game(name='with Mona-chan'))
     print(f'{bot.user.name} is connected to Discord :D')
 
 
@@ -27,9 +31,29 @@ async def on_member_join(member):
 
 
 @bot.command(name='time', help='tells current pst time')
-async def time(ctx):
-    current_time = datetime.now().strftime("%H:%M:%S")
-    await ctx.send(current_time)
+async def time(ctx, *args):
+    if len(args) == 0:
+        return
+    elif len(args) > 0:
+        try:
+            tz1 = pytz.timezone(tz_names[args[0].upper()])
+        except KeyError:
+            await ctx.send("Not a valid timezone")
+            return
+        date_time = datetime.now(tz1)
+        if len(args) == 1:
+            current_time1 = date_time.strftime("%H:%M:%S")
+            await ctx.send(current_time1)
+        elif len(args) == 3:
+            # convert given time og tz1 to tz2
+            try:
+                tz2 = pytz.timezone(tz_names[args[1].upper()])
+            except KeyError:
+                await ctx.send("Not a valid timezone")
+            date_time2 = datetime.now(tz2)
+            pass
+        else:
+            return
 
 
 @bot.event
@@ -39,5 +63,7 @@ async def on_message(message):
         return
     if 'tadaima' in message.content.lower():
         await message.channel.send("Okaeri!")
+    elif 'otsu' in message.content.lower():
+        await message.channel.send("Otsukare!")
 
 bot.run(TOKEN)

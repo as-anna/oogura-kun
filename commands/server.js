633a4c92406b0114e45f85542e437d90c1b3d52a
Anna A <patty.asprer@gmail.com>
2019-12-26 16:40:44 -0800

    Added simple server info command

---
+++ commands/server.js
@@ -0,0 +1,10 @@
+module.exports = {
+    name: 'server',
+    description: 'Displays server info',
+    execute(message, args) {
+        message.channel.send(`Name: ${message.guild.name}\n`
+                            + `Region: ${message.guild.region}\n`
+                            + `Total Member Count: ${message.guild.memberCount}\n`
+                            + `Created On: ${message.guild.createdAt}`);
+    }
+};
--- index.js
+++ index.js
@@ -1,17 +1,32 @@
+const fs = require('fs');
 const Discord = require('discord.js');
 const { prefix, token } = require('./config.json');
 
 const client = new Discord.Client();
+client.commands = new Discord.Collection();
 
-client.on('message', (msg) => {
+const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
 
-    if (message.author.bot) return;
+for (const file of commandFiles) {
+	const command = require(`./commands/${file}`);
+	client.commands.set(command.name, command);
+}
 
-    const args = message.content.slice(prefix.length).split(' ');
-    const command = args.shift().toLowerCase();
+client.on('message', (message) => {
+    if (!message.content.startsWith(prefix) || message.author.bot){
+        if (message.content.toLowerCase().includes('tadaima') && !message.author.bot){
+            message.channel.send(`Okaeri!`);
+        }
+        return;
+    }
 
-    if (msg.content.toLowerCase().includes('tadaima')){
-        msg.channel.send(`Okaeri!`);
+	const args = message.content.slice(prefix.length).split(/ +/);
+	const command = args.shift().toLowerCase();
+
+    if (command === 'ping') {
+		client.commands.get('ping').execute(message, args);
+	} else if (command === 'server') {
+        client.commands.get('server').execute(message, args);
     }
 });
 
